a09a24727005e965533412225fabdf12aacd49e2
YoungGuy2003 <abhijithts402@gmail.com>
2024-07-01 21:00:07 +0530

    made changes in the navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import './Nav.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <img src="/logo.png" alt="Krudcup Logo" />
+        <Link to="/">
+          <img src="/logo.png" alt="Krudcup Logo" />
+        </Link>
+      </div>
+      
+      <div className="navbar-title">
         <span>Watch4Deal</span>
       </div>
       
       <div className="navbar-icons">
-        
-        
       </div>
     </nav>
   );
